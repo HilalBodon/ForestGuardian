9ce378e43d136d873a0749d1b7c4cc8445c7b08a
HilalBodon <hilal_bodon@hotmail.com>
2023-09-17 00:16:15 +0300

    adding animation to side menu

--- client/src/components/Main/index.jsx
+++ client/src/components/Main/index.jsx
@@ -1,114 +1,4 @@
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
-// import BookCard from "../BookCard";
-// import AddBookForm from "../AddBookForm";
-// import jwt_decode from "jwt-decode";
-// import RecommendedBooks from "../RecommendedBooks";
-// import BookDiscovery from "../Search/search";
-// import styles from "./styles.module.css";
-
-// const Main = ({ authToken }) => {
-//   const [userId, setUserId] = useState(null);
-//   const [showAddBookForm, setShowAddBookForm] = useState(true);
-//   const [books, setBooks] = useState([]);
-//   const [showRecommended, setShowRecommended] = useState(false);
-//   const [showBookDiscovery, setShowBookDiscovery] = useState(true);
-
-//   const handleLogout = () => {
-//     localStorage.removeItem("token");
-//     window.location.reload();
-//   };
-
-//   useEffect(() => {
-//     const decodedToken = jwt_decode(authToken);
-//     const fetchedUserId = decodedToken._id;
-//     setUserId(fetchedUserId);
-
-//     const fetchBooks = async () => {
-//       try {
-//         const response = await axios.get("http://localhost:8080/api/books", {
-//           headers: {
-//             Authorization: `Bearer ${authToken}`,
-//           },
-//         });
-//         setBooks(response.data);
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
-//     fetchBooks();
-//   }, [authToken]);
-
-//   const handleBookAdded = (newBook) => {
-//     setBooks([...books, newBook]);
-//   };
-
-//   return (
-//     <div className={styles.main_container}>
-//       <nav className={styles.navbar}>
-//         <h1>ShareBook</h1>
-//         <button
-//           className={styles.btn}
-//           onClick={() => {
-//             setShowAddBookForm(!showAddBookForm);
-//           }}
-//         >
-//           + Add New Book
-//         </button>
-//         <button
-//           className={styles.btn}
-//           onClick={() => {
-//             setShowRecommended(!showRecommended);
-//           }}
-//         >
-//           {showRecommended ? "All Books" : "Recommended Books"}
-//         </button>
-//         <button className={styles.white_btn} onClick={handleLogout}>
-//           Logout
-//         </button>
-//       </nav>
-
-
-// {showBookDiscovery && (
-//         <div className={styles.search_add}>
-//           <BookDiscovery authToken={authToken}/>
-		  
-//         </div>
-//       )}
-	  
-//       {!showAddBookForm && (
-// 				<AddBookForm
-// 		authToken={authToken}
-// 		onBookAdded={handleBookAdded}
-// 		setShowAddBookForm={setShowAddBookForm}
-// 		/>
-//       )}
-
-//       <div className={styles.books_container}>
-//         {showRecommended ? (
-//           <RecommendedBooks authToken={authToken} />
-//         ) : (
-//           books.map((book) => (
-//             <BookCard
-//               key={book._id}
-//               book={book}
-//               authToken={authToken}
-//               userId={userId}
-//             />
-//           ))
-//         )}
-//       </div>
-
-
-//     </div>
-//   );
-// };
-
-// export default Main;
-
-
-
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from 'react';
 import axios from "axios";
 import BookCard from "../BookCard";
 import AddBookForm from "../AddBookForm";
@@ -128,6 +18,8 @@ const Main = ({ authToken }) => {
   const [showBookDiscovery, setShowBookDiscovery] = useState(true);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
+  const sideMenuRef = useRef(null);
+
 
 
   // const handleLogout = () => {
@@ -174,35 +66,57 @@ const Main = ({ authToken }) => {
   const closeSideMenu = () => {
     setIsSideMenuOpen(false);
   };
-  
+
+  useEffect(() => {
+    const handleOutsideClick = (e) => {
+      if (isSideMenuOpen && !sideMenuRef.current.contains(e.target)) {
+        closeSideMenu();
+      }
+    };
+
+    if (isSideMenuOpen) {
+      window.addEventListener('click', handleOutsideClick);
+    } else {
+      window.removeEventListener('click', handleOutsideClick);
+    }
+    return () => {
+      window.removeEventListener('click', handleOutsideClick);
+    };
+  }, [isSideMenuOpen]);
+
+
+
   return (
 
     <div className={styles.main_container}>
       <nav className={styles.navbar}>
-      <p className={styles.menuIcon}><FaBars onClick={toggleSideMenu} /></p>
+        <p className={styles.menuIcon}><FaBars onClick={toggleSideMenu} /></p>
+        {isSideMenuOpen && (
+          <div className={styles.overlay}></div>
+        )}
 
-      {isSideMenuOpen && (
-              <div className={styles.sideMenu}>
-                <div className={styles.sideMenuContent}>
-                  <a href="#" onClick={closeSideMenu} className={styles.sideMenuItem}>
-                    Add Device
-                  </a>
-                  <a href="#" onClick={closeSideMenu} className={styles.sideMenuItem}>
-                    About Us
-                  </a>
-                  <a href="#" onClick={closeSideMenu} className={styles.sideMenuItem}>
-                    How to Use App
-                  </a>
-                  <a href="#" onClick={closeSideMenu} className={styles.sideMenuItem}>
-                    Account Settings
-                  </a>
-                  <a href="#" onClick={closeSideMenu} className={styles.sideMenuItem}>
-                    Logout
-                  </a>
-                </div>
+            <div
+              ref={sideMenuRef}
+              className={`${styles.sideMenu} ${isSideMenuOpen ? styles.open : ''}`}>
+              <div className={styles.sideMenuContent}>
+                <a href="#" onClick={closeSideMenu} className={styles.sideMenuItem}>
+                  Add Device
+                </a>
+                <a href="#" onClick={closeSideMenu} className={styles.sideMenuItem}>
+                  About Us
+                </a>
+                <a href="#" onClick={closeSideMenu} className={styles.sideMenuItem}>
+                  How to Use App
+                </a>
+                <a href="#" onClick={closeSideMenu} className={styles.sideMenuItem}>
+                  Account Settings
+                </a>
+                <a href="#" onClick={closeSideMenu} className={styles.sideMenuItem}>
+                  Logout
+                </a>
               </div>
-            )}
-            
+            </div>
+
           <div className={styles.logoDot}>
           <div><img className={styles.nameLogo} src={nameLogo} alt="NameLogo" /></div>
           <div className={styles.dotesIconDiv}>
@@ -240,6 +154,8 @@ const Main = ({ authToken }) => {
 };
 
 export default Main;
+
+
 
         {/* <button
           className={styles.btn}
