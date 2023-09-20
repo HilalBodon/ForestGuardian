bd20be49151fa39a1ad180d06645b5e7a5c697e4
HilalBodon <hilal_bodon@hotmail.com>
2023-09-20 22:45:10 +0300

    fix device card size

--- client/src/components/Main/HowToUse/index.jsx
+++ client/src/components/Main/HowToUse/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./HowToUse.css";
-import greenLogo from "../../../assets/images/";
+import deviceImg from "../../../assets/images/deviceImg.png";
+import onTreeDevice from "../../../assets/images/onTreeDevice.png"; 
+
 
 
 const HowToUse= ({handleBackClick}) => {
@@ -11,8 +13,9 @@ return(
         <p>1.First you have to Contact us to provide you with 
             the hardware responsible of collecting the needed data and 
             triggering the logging notification.</p>
-            <img  src={greenLogo} alt="Green Guardian Logo" /> 
+            <img  src={deviceImg} alt="Device Image" /> 
         <p>2.Hide the hardware in a place near to your target. </p>
+            <img  src={onTreeDevice} alt="On Tree Device Image" /> 
 
         <p>3.Make sure you have a near WIFI network to enable the device connection (you can check the WIFI SSID(name) and signal strength from you mobile ).
 Add new device and provide the needed information.</p>
