4711e8677891b1dd8c2b45beab51be4d0d4e3e7c
HilalBodon <hilal_bodon@hotmail.com>
2023-09-21 02:14:00 +0300

    moving the icons

--- client/src/components/Main/AccountSettings/index.jsx
+++ client/src/components/Main/AccountSettings/index.jsx
@@ -60,56 +60,63 @@ import {MdAlternateEmail} from "react-icons/md";
 import {BsFillTelephoneFill} from "react-icons/bs"; 
 import {BiSolidEditAlt} from "react-icons/bi"; 
 const AccountSettings = ({ handleBackClick }) => {
+
   return (
+
     <div className={styles.accountSettingsContainer}>
+      <FaUserAlt className={styles.icon}/>
+
+      <RiLockPasswordFill className={styles.icon}/>
+
+      <BsFillQuestionSquareFill className={styles.icon}/>
+
+      <MdAlternateEmail className={styles.icon}/>
+
+      <BsFillTelephoneFill className={styles.icon}/>
 
     <div className={iconsDiv}>
 
     </div>
+
+
     <div className={labelsDiv}>
 
     </div>
+
+
     <div className={inputsDiv}>
 
     </div>
+
+
     <div className={editDiv}>
-      
+
     </div>
 
 
-
-
-
-
-
       <div className={styles.settingItem}>
-      <FaUserAlt className={styles.icon}/>
         <label htmlFor="userId">User ID</label>
         <input type="text" id="userId" name="userId" />
       </div>
       <div className={styles.settingItem}>
-      <RiLockPasswordFill className={styles.icon}/>
         <label htmlFor="password">Password</label>
         <input type="password" id="password" name="password" />
         <BiSolidEditAlt className={styles.editIcon}/>
 
       </div>
       <div className={styles.settingItem}>
-      <BsFillQuestionSquareFill className={styles.icon}/>
         <label htmlFor="fullname">Full Name</label>
         <input type="text" id="fullname" name="fullname" />
         <BiSolidEditAlt className={styles.editIcon}/>
 
       </div>
       <div className={styles.settingItem}>
-      <MdAlternateEmail className={styles.icon}/>
         <label htmlFor="email">Email</label>
         <input type="email" id="email" name="email" />
         <BiSolidEditAlt className={styles.editIcon}/>
 
       </div>
       <div className={styles.settingItem}>
-      <BsFillTelephoneFill className={styles.icon}/>
         <label htmlFor="phone">Phone</label>
         <input type="tel" id="phone" name="phone" />
         <BiSolidEditAlt className={styles.editIcon}/>
