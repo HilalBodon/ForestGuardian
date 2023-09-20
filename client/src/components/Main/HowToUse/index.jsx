c1fef25694b89d0c98b84a7ecb5521d1ab719ceb
HilalBodon <hilal_bodon@hotmail.com>
2023-09-20 22:20:19 +0300

    add how to use / images

--- client/src/components/Main/HowToUse/index.jsx
+++ client/src/components/Main/HowToUse/index.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import "./HowToUse.css";
+import greenLogo from "../../../assets/images/";
+
 
 const HowToUse= ({handleBackClick}) => {
 
 return(
 
     <div className="mainContainer">
-        <p>1. First you have to Contact us to provide you with 
+        <p>1.First you have to Contact us to provide you with 
             the hardware responsible of collecting the needed data and 
             triggering the logging notification.</p>
+            <img  src={greenLogo} alt="Green Guardian Logo" /> 
+        <p>2.Hide the hardware in a place near to your target. </p>
 
-        <p>2.hide the hardware in a place near to your target. </p>
+        <p>3.Make sure you have a near WIFI network to enable the device connection (you can check the WIFI SSID(name) and signal strength from you mobile ).
+Add new device and provide the needed information.</p>
         <button  className="button" onClick={handleBackClick}>Back</button>
     
     </div>
