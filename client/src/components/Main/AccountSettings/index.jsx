5272fd7a658f75323e21a2ec1d2726a41dc37699
HilalBodon <hilal_bodon@hotmail.com>
2023-09-21 01:20:29 +0300

    adding the rest icons

--- client/src/components/Main/AccountSettings/index.jsx
+++ client/src/components/Main/AccountSettings/index.jsx
@@ -2,36 +2,36 @@ import React from 'react';
 import styles from "./AccountSettings.module.css";
 import {FaUserAlt} from "react-icons/fa";
 import {RiLockPasswordFill} from "react-icons/ri";
-import {BsFillQuestionSquareFill} from "react-icons/bs";  
+import {BsFillQuestionSquareFill} from "react-icons/bs"; 
+import {MdAlternateEmail} from "react-icons/md"; 
+import {BsFillTelephoneFill} from "react-icons/bs"; 
+
 const AccountSettings = ({ handleBackClick }) => {
   return (
     <div className={styles.accountSettingsContainer}>
-      <div className={styles.iconDiv}>
-      <FaUserAlt className={styles.icon}/>
-      <RiLockPasswordFill className={styles.icon}/>
-      <BsFillQuestionSquareFill className={styles.icon}/>
-      <BsFillQuestionSquareFill className={styles.icon}/>
-      <BsFillQuestionSquareFill className={styles.icon}/>
-      <BsFillQuestionSquareFill className={styles.icon}/>
 
-      </div>
       <div className={styles.settingItem}>
+      <FaUserAlt className={styles.icon}/>
         <label htmlFor="userId">User ID</label>
         <input type="text" id="userId" name="userId" />
       </div>
       <div className={styles.settingItem}>
+      <RiLockPasswordFill className={styles.icon}/>
         <label htmlFor="password">Password</label>
         <input type="password" id="password" name="password" />
       </div>
       <div className={styles.settingItem}>
+      <BsFillQuestionSquareFill className={styles.icon}/>
         <label htmlFor="fullname">Full Name</label>
         <input type="text" id="fullname" name="fullname" />
       </div>
       <div className={styles.settingItem}>
+      <MdAlternateEmail className={styles.icon}/>
         <label htmlFor="email">Email</label>
         <input type="email" id="email" name="email" />
       </div>
       <div className={styles.settingItem}>
+      <BsFillTelephoneFill className={styles.icon}/>
         <label htmlFor="phone">Phone</label>
         <input type="tel" id="phone" name="phone" />
       </div>
