fdafa75d80db39de982689a743e949862fe26678
HilalBodon <hilal_bodon@hotmail.com>
2023-09-21 03:17:02 +0300

    cleaning the pAGE

--- client/src/components/Main/AccountSettings/index.jsx
+++ client/src/components/Main/AccountSettings/index.jsx
@@ -1,56 +1,3 @@
-// import React from 'react';
-// import styles from "./AccountSettings.module.css";
-// import {FaUserAlt} from "react-icons/fa";
-// import {RiLockPasswordFill} from "react-icons/ri";
-// import {BsFillQuestionSquareFill} from "react-icons/bs"; 
-// import {MdAlternateEmail} from "react-icons/md"; 
-// import {BsFillTelephoneFill} from "react-icons/bs"; 
-// import {BiSolidEditAlt} from "react-icons/bi"; 
-// const AccountSettings = ({ handleBackClick }) => {
-//   return (
-//     <div className={styles.accountSettingsContainer}>
-
-//       <div className={styles.settingItem}>
-//       <FaUserAlt className={styles.icon}/>
-//         <label htmlFor="userId">User ID</label>
-//         <input type="text" id="userId" name="userId" />
-//       </div>
-//       <div className={styles.settingItem}>
-//       <RiLockPasswordFill className={styles.icon}/>
-//         <label htmlFor="password">Password</label>
-//         <input type="password" id="password" name="password" />
-//         <BiSolidEditAlt className={styles.editIcon}/>
-
-//       </div>
-//       <div className={styles.settingItem}>
-//       <BsFillQuestionSquareFill className={styles.icon}/>
-//         <label htmlFor="fullname">Full Name</label>
-//         <input type="text" id="fullname" name="fullname" />
-//         <BiSolidEditAlt className={styles.editIcon}/>
-
-//       </div>
-//       <div className={styles.settingItem}>
-//       <MdAlternateEmail className={styles.icon}/>
-//         <label htmlFor="email">Email</label>
-//         <input type="email" id="email" name="email" />
-//         <BiSolidEditAlt className={styles.editIcon}/>
-
-//       </div>
-//       <div className={styles.settingItem}>
-//       <BsFillTelephoneFill className={styles.icon}/>
-//         <label htmlFor="phone">Phone</label>
-//         <input type="tel" id="phone" name="phone" />
-//         <BiSolidEditAlt className={styles.editIcon}/>
-
-//       </div>
-//       <button className={styles.button} onClick={handleBackClick}>Back</button>
-//     </div>
-//   );
-// };
-
-// export default AccountSettings;
-
-
 import React from 'react';
 import styles from "./AccountSettings.module.css";
 import {FaUserAlt} from "react-icons/fa";
@@ -75,7 +22,7 @@ const AccountSettings = ({ handleBackClick }) => {
 
 
     <div className={styles.labelsDiv}>
-      <label htmlFor="userId">User ID</label>
+      <label htmlFor="userEmail">User Email</label>
       <label htmlFor="password">Password</label>
       <label htmlFor="fullname">Full Name</label>
       <label htmlFor="email">Email</label>
@@ -84,7 +31,7 @@ const AccountSettings = ({ handleBackClick }) => {
 
 
     <div className={styles.inputsDiv}>
-      <input type="text" id="userId" name="userId" />
+      <input type="text" id="userEmail" name="userEmail" />
       <input type="password" id="password" name="password" />
       <input type="text" id="fullname" name="fullname" />
       <input type="email" id="email" name="email" />
