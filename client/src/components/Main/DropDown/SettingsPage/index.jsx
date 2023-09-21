c1b1c2c329666c80f12ff3a2c48b228776a9dadf
HilalBodon <hilal_bodon@hotmail.com>
2023-09-21 04:36:17 +0300

    fix toggle on off

--- client/src/components/Main/DropDown/SettingsPage/index.jsx
+++ client/src/components/Main/DropDown/SettingsPage/index.jsx
@@ -1,72 +1,50 @@
-
 import React, { useState } from 'react';
 import styles from "./SettingsStyle.module.css";
-import {BsFillQuestionSquareFill} from "react-icons/bs"; 
+import { BsFillQuestionSquareFill } from "react-icons/bs"; 
 import { BsToggleOn, BsToggleOff } from 'react-icons/bs';
-import {MdNotificationsActive} from 'react-icons/md';
-import {IoMdNotificationsOff} from 'react-icons/io';
-import {GiAxeInLog} from 'react-icons/gi';
-import {GiSawedOffShotgun} from 'react-icons/gi';
+import { MdNotificationsActive } from 'react-icons/md';
+import { IoMdNotificationsOff } from 'react-icons/io';
+import { GiAxeInLog } from 'react-icons/gi';
+import { GiSawedOffShotgun } from 'react-icons/gi';
 
 const SettingsPage = ({ handleBackClick }) => {
+  const [toggleStates, setToggleStates] = useState([false, false, false, false, false]);
 
-  const [isToggled, setIsToggled] = useState(false);
-
-  const toggleIcon = () => {
-    setIsToggled(!isToggled);
+  const toggleIcon = (index) => {
+    const newToggleStates = [...toggleStates];
+    newToggleStates[index] = !newToggleStates[index];
+    setToggleStates(newToggleStates);
   };
 
   return (
     <div className={styles.mainDiv}>
-    <div className={styles.accountSettingsContainer}>
+      <div className={styles.accountSettingsContainer}>
+        <div className={styles.iconsDiv}>
+          <MdNotificationsActive className={styles.icon} />
+          <IoMdNotificationsOff className={styles.icon} />
+          <BsFillQuestionSquareFill className={styles.icon} />
+          <GiAxeInLog className={styles.icon} />
+          <GiSawedOffShotgun className={styles.icon} />
+        </div>
 
-    <div className={styles.iconsDiv}>
-      <MdNotificationsActive className={styles.icon}/>
-      <IoMdNotificationsOff className={styles.icon}/>
-      <BsFillQuestionSquareFill className={styles.icon}/>
-      <GiAxeInLog className={styles.icon}/>
-      <GiSawedOffShotgun className={styles.icon}/>
-    </div>
+        <div className={styles.labelsDiv}>
+          <label>Notify similar sounds</label>
+          <label>Notify E-mail after 5 min</label>
+          <label>Enable live confirmation</label>
+          <label disabled>Axe Sound detection</label>
+          <label>Gun Sound detection</label>
+        </div>
 
-
-    <div className={styles.labelsDiv}>
-      <label>Notify similar sounds</label>
-      <label>Notify E-mail after 5 min </label>
-      <label >Enable live confirmation</label>
-      <label disabled>Axe Sound detection</label>
-      <label>Gun Sound detection</label>
-    </div>
-
-
-    <div className={styles.toggleDiv}>
-      {isToggled ? (
-        <BsToggleOn className={styles.toggleIcon} onClick={toggleIcon} />
-      ) : (
-        <BsToggleOff className={styles.toggleIcon} onClick={toggleIcon} />
-      )}
-      {isToggled ? (
-        <BsToggleOn className={styles.toggleIcon} onClick={toggleIcon} />
-      ) : (
-        <BsToggleOff className={styles.toggleIcon} onClick={toggleIcon} />
-      )}
-      {isToggled ? (
-        <BsToggleOn className={styles.toggleIcon} onClick={toggleIcon} />
-      ) : (
-        <BsToggleOff className={styles.toggleIcon} onClick={toggleIcon} />
-      )}
-      {isToggled ? (
-        <BsToggleOn className={styles.toggleIcon} onClick={toggleIcon} />
-      ) : (
-        <BsToggleOff className={styles.toggleIcon} onClick={toggleIcon} />
-      )}
-      {isToggled ? (
-        <BsToggleOn className={styles.toggleIcon} onClick={toggleIcon} />
-      ) : (
-        <BsToggleOff className={styles.toggleIcon} onClick={toggleIcon} />
-      )}
-    </div>
-
-    </div>
+        <div className={styles.toggleDiv}>
+          {toggleStates.map((isToggled, index) => (
+            isToggled ? (
+              <BsToggleOn key={index} className={styles.toggleIcon} onClick={() => toggleIcon(index)} />
+            ) : (
+              <BsToggleOff key={index} className={styles.toggleIcon} onClick={() => toggleIcon(index)} />
+            )
+          ))}
+        </div>
+      </div>
       <button className={styles.button} onClick={handleBackClick}>Back</button>
     </div>
   );
