5eb77d147909e61816a21f36f92e5fa22fa67d94
HilalBodon <hilal_bodon@hotmail.com>
2023-09-23 23:38:46 +0300

    add date format

--- server/routes/notifications.js
+++ server/routes/notifications.js
@@ -7,30 +7,17 @@ const { format } = require('date-fns');
 
 // __________________________________________get all notifications
 
-// router.get('/', async (req, res) => {
-//     try {
-//       const notifications = await Notification.find()
-//         .exec();
-  
-//       res.status(200).json(notifications);
-//     } catch (error) {
-//       console.error(error);
-//       res.status(500).json({ error: 'Internal server error' });
-//     }
-//   });
 
 
 router.get('/', async (req, res) => {
   try {
       const notifications = await Notification.find().exec();
 
-      // Map notifications to include formatted createdAt
       const formattedNotifications = notifications.map(notification => ({
           _id: notification._id,
           device: notification.device,
           user: notification.user,
           message: notification.message,
-          // Format createdAt as "DD/MM/YYYY" using date-fns
           createdAt: format(new Date(notification.createdAt), 'dd/MM/yyyy hh:mm:ss'),
           __v: notification.__v
       }));
