3969923d227af66db1397cf798673d646a4cc509
HilalBodon <hilal_bodon@hotmail.com>
2023-09-23 23:55:40 +0300

    get notification by user id

--- server/routes/notifications.js
+++ server/routes/notifications.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Notification = require('../models/notification');
 const Device = require('../models/device');
-const User = require('../models/user');
+const { User } = require('../models/user');
 const { format } = require('date-fns');
 
 // __________________________________________get all notifications
@@ -42,6 +42,38 @@ router.post('/', async (req, res) => {
     await notification.save();
 
     res.status(201).json(notification);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// _______________________________________________________________
+// get notifications of the user :
+
+
+router.get('/:userId', async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    const user = await User.findById(userId).exec();
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    const notifications = await Notification.find({ user: userId }).exec();
+
+    const formattedNotifications = notifications.map(notification => ({
+      _id: notification._id,
+      device: notification.device,
+      user: notification.user,
+      message: notification.message,
+      createdAt: format(new Date(notification.createdAt), 'dd/MM/yyyy hh:mm:ss'),
+      __v: notification.__v
+    }));
+
+    res.status(200).json(formattedNotifications);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal server error' });
