9adc2e2e2defcd3961da57ca46143b4a176e54f3
HilalBodon <hilal_bodon@hotmail.com>
2023-09-24 15:02:18 +0300

    toggle with edit icons

--- client/src/Pages/AccountSettings/index.jsx
+++ client/src/Pages/AccountSettings/index.jsx
@@ -1,55 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./AccountSettings.module.css";
-import {FaUserAlt} from "react-icons/fa";
-import {RiLockPasswordFill} from "react-icons/ri";
-import {BsFillQuestionSquareFill} from "react-icons/bs"; 
-import {MdAlternateEmail} from "react-icons/md"; 
-import {BsFillTelephoneFill} from "react-icons/bs"; 
-import {BiSolidEditAlt} from "react-icons/bi"; 
+import { FaUserAlt } from "react-icons/fa";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { BsFillQuestionSquareFill } from "react-icons/bs";
+import { MdAlternateEmail } from "react-icons/md";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { BiSolidEditAlt } from "react-icons/bi";
 
-const AccountSettings = ({  userData ,handleBackClick}) => {
-console.log(userData);
-const { email, fullName, password ,notifyEmail,phoneNumber} = userData.user;
+const AccountSettings = ({ userData, handleBackClick }) => {
+  const { email, fullName, password, notifyEmail, phoneNumber } = userData.user;
+
+  // Create state variables for each input field
+  const [isEmailEditing, setEmailEditing] = useState(false);
+  const [isPasswordEditing, setPasswordEditing] = useState(false);
+  const [isFullNameEditing, setFullNameEditing] = useState(false);
+  const [isNotifyEmailEditing, setNotifyEmailEditing] = useState(false);
+  const [isPhoneNumberEditing, setPhoneNumberEditing] = useState(false);
+
+  const handleEditClick = (fieldName) => {
+    // Toggle the editing state for the corresponding field
+    switch (fieldName) {
+      case 'password':
+        setPasswordEditing(!isPasswordEditing);
+        break;
+      case 'fullName':
+        setFullNameEditing(!isFullNameEditing);
+        break;
+      case 'notifyEmail':
+        setNotifyEmailEditing(!isNotifyEmailEditing);
+        break;
+      case 'phoneNumber':
+        setPhoneNumberEditing(!isPhoneNumberEditing);
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
     <div className={styles.mainDiv}>
-    <div className={styles.accountSettingsContainer}>
-    <div className={styles.iconsDiv}>
-      <FaUserAlt className={styles.icon}/>
-      <RiLockPasswordFill className={styles.icon}/>
-      <BsFillQuestionSquareFill className={styles.icon}/>
-      <MdAlternateEmail className={styles.icon}/>
-      <BsFillTelephoneFill className={styles.icon}/>
-    </div>
+      <div className={styles.accountSettingsContainer}>
+        <div className={styles.iconsDiv}>
+          <FaUserAlt className={styles.icon} />
+          <RiLockPasswordFill className={styles.icon} />
+          <BsFillQuestionSquareFill className={styles.icon} />
+          <MdAlternateEmail className={styles.icon} />
+          <BsFillTelephoneFill className={styles.icon} />
+        </div>
 
+        <div className={styles.labelsDiv}>
+          <label htmlFor="userEmail">User Email</label>
+          <label htmlFor="password">Password</label>
+          <label htmlFor="fullname">Full Name</label>
+          <label htmlFor="email">Email</label>
+          <label htmlFor="phone">Phone</label>
+        </div>
 
-    <div className={styles.labelsDiv}>
-      <label htmlFor="userEmail">User Email</label>
-      <label htmlFor="password">Password</label>
-      <label htmlFor="fullname">Full Name</label>
-      <label htmlFor="email">Email</label>
-      <label htmlFor="phone">Phone</label>
-    </div>
+        <div className={styles.inputsDiv}>
+          <input type="text" id="userEmail" name="userEmail" defaultValue={email} disabled/>
+          <input type="password" id="password" name="password" defaultValue={password} disabled={!isPasswordEditing} />
+          <input type="text" id="fullname" name="fullname" defaultValue={fullName} disabled={!isFullNameEditing} />
+          <input type="email" id="email" name="email" defaultValue={notifyEmail} disabled={!isNotifyEmailEditing} />
+          <input type="tel" id="phone" name="phone" defaultValue={phoneNumber} disabled={!isPhoneNumberEditing} />
+        </div>
 
-
-    <div className={styles.inputsDiv}>
-          <input type="text" id="userEmail" name="userEmail" defaultValue={email} disabled />
-          <input type="password" id="password" name="password" defaultValue={password} />
-          <input type="text" id="fullname" name="fullname" defaultValue={fullName} />
-          <input type="email" id="email" name="email" defaultValue={notifyEmail} />
-          <input type="tel" id="phone" name="phone" defaultValue={phoneNumber} />
-    </div>
-
-
-    <div className={styles.editDiv}>
-      <p></p>
-      <BiSolidEditAlt className={styles.editIcon}/>
-      <BiSolidEditAlt className={styles.editIcon}/>
-      <BiSolidEditAlt className={styles.editIcon}/>
-      <BiSolidEditAlt className={styles.editIcon}/>
-    </div>
-
-    </div>
+        <div className={styles.editDiv}>
+          <p></p>
+          <BiSolidEditAlt className={styles.editIcon} onClick={() => handleEditClick('password')} />
+          <BiSolidEditAlt className={styles.editIcon} onClick={() => handleEditClick('fullName')} />
+          <BiSolidEditAlt className={styles.editIcon} onClick={() => handleEditClick('notifyEmail')} />
+          <BiSolidEditAlt className={styles.editIcon} onClick={() => handleEditClick('phoneNumber')} />
+        </div>
+      </div>
       <button className={styles.button} onClick={handleBackClick}>Back</button>
     </div>
   );
