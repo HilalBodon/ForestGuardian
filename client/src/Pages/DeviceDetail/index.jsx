868de72f7b9c72a1b9adbf4eba7b61c7acc12457
HilalBodon <hilal_bodon@hotmail.com>
2023-09-25 23:15:52 +0300

    add desihn to device detail

--- client/src/Pages/DeviceDetail/index.jsx
+++ client/src/Pages/DeviceDetail/index.jsx
@@ -5,19 +5,14 @@ import AudioClassification from "../../components/AudioClassification";
 const DeviceDetail = ({ device, userId, handleBackClick }) => {
   return (
     <div className="device-detail-container">
-      <button className="button" onClick={handleBackClick}>
+      <button className="button addMargin" onClick={handleBackClick}>
         Back
       </button>
 
-        <div key={device._id}>
-          <p>Device Name: {device.deviceName}</p>
-          <p>Device ID: {device._id}</p>
-          <p>User ID: {userId}</p>
-          <p>Tree Type: {device.treeType}</p>
-          <p>Tree Height: {device.treeHeight}</p>
-          <p>Location: {device.location}</p>
-          <p>Details: {device.details}</p>
-          <p>Picture: {device.picture}</p>
+            <img src={device.picture} alt="Device Image" />        <div key={device._id}>
+          <p>{device.deviceName} is attached to a {device.treeType} 
+          tree, With Approximate Height of: {device.treeHeight} meters, in {device.location}</p>
+          <p>More details: {device.details}</p>
 
           <AudioClassification userId={userId} deviceId={device._id} />
         </div>
