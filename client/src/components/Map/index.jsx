4b304852b3b08052ec2920db0b3aa99c1d13ba4c
HilalBodon <hilal_bodon@hotmail.com>
2023-09-28 00:47:12 +0300

    the map work with values

--- client/src/components/Map/index.jsx
+++ client/src/components/Map/index.jsx
@@ -23,33 +23,33 @@
   
 
 
-  import React, { useRef, useEffect } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+//   import React, { useRef, useEffect } from "react";
+// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
-function LeafletMap(props) {
-  const { selectedLocation } = props;
-  const defaultCenter = [34.43352, 35.84415]; 
-  const mapRef = useRef();
+// function LeafletMap(props) {
+//   const { selectedLocation } = props;
+//   const defaultCenter = [34.43352, 35.84415]; 
+//   const mapRef = useRef();
 
-  useEffect(() => {
-    if (selectedLocation && mapRef.current) {
-      mapRef.current.flyTo(selectedLocation, 10, {
-        duration: 3,
-      });
-    }
-  }, [selectedLocation]);
-  return (
-    <MapContainer ref={mapRef} center={selectedLocation || defaultCenter} zoom={10} style={{ height: "400px" ,width:"500px"}}>
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      {selectedLocation && selectedLocation.lat && selectedLocation.lng && (
-        <Marker position={selectedLocation}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      )}
-    </MapContainer>
-  );
-}
+//   useEffect(() => {
+//     if (selectedLocation && mapRef.current) {
+//       mapRef.current.flyTo(selectedLocation, 10, {
+//         duration: 3,
+//       });
+//     }
+//   }, [selectedLocation]);
+//   return (
+//     <MapContainer ref={mapRef} center={selectedLocation || defaultCenter} zoom={10} style={{ height: "400px" ,width:"500px"}}>
+//       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+//       {selectedLocation && selectedLocation.lat && selectedLocation.lng && (
+//         <Marker position={selectedLocation}>
+//           <Popup>
+//             A pretty CSS3 popup. <br /> Easily customizable.
+//           </Popup>
+//         </Marker>
+//       )}
+//     </MapContainer>
+//   );
+// }
 
-export default LeafletMap;
+// export default LeafletMap;
