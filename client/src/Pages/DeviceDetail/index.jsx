a3759e5a7102b6e31b7ac33266c32d9958137e9a
HilalBodon <hilal_bodon@hotmail.com>
2023-09-28 00:19:42 +0300

    the map work with values

--- client/src/Pages/DeviceDetail/index.jsx
+++ client/src/Pages/DeviceDetail/index.jsx
@@ -4,7 +4,25 @@ import AudioClassification from "../../components/AudioClassification";
 import CustomMap from "../../components/leaflet";
 
 const DeviceDetail = ({ device, userId, handleBackClick }) => {
-    const [latitude, longitude] = device.location.split(',').map(parseFloat);
+  let latitude = 0;
+  let longitude = 0;
+  let error = null;
+
+  if (device.location && /^\d+\.\d+,\s*\d+\.\d+$/.test(device.location)) {
+    const [latStr, lonStr] = device.location.split(',').map((str) => str.trim());
+    
+    const lat = parseFloat(latStr);
+    const lon = parseFloat(lonStr);
+
+    if (!isNaN(lat) && !isNaN(lon)) {
+      latitude = lat;
+      longitude = lon;
+    } else {
+      error = "Invalid latitude or longitude values in device.location";
+    }
+  } else {
+    error = "Invalid device.location format";
+  }
 
   return (
     <div className="device-detail-container">
@@ -12,19 +30,28 @@ const DeviceDetail = ({ device, userId, handleBackClick }) => {
         Back
       </button>
 
-            <img src={device.picture} alt="Device Image" />        <div key={device._id}>
-          <p>{device.deviceName} is attached to a {device.treeType} 
-          tree, With Approximate Height of: {device.treeHeight} meters, in {device.location}</p>
-          <p>More details: {device.details}</p>
-          <p>{device.location}</p>
-
-          <AudioClassification userId={userId} deviceId={device._id} />
-          <CustomMap latitude={latitude} longitude={longitude} />
-
-
-        </div>
+      <img src={device.picture} alt="Device Image" />
+      <div key={device._id}>
+        <p>
+          {device.deviceName} is attached to a {device.treeType} tree, With
+          Approximate Height of: {device.treeHeight} meters, in{" "}
+          {device.location}
+        </p>
+        <p>More details: {device.details}</p>
+        {error ? (
+          <p className="error-message">{error}</p>
+        ) : (
+          <div>
+            <p>Latitude: {latitude}</p>
+            <p>Longitude: {longitude}</p>
+            <AudioClassification userId={userId} deviceId={device._id} />
+            <CustomMap latitude={latitude} longitude={longitude} />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
 
 export default DeviceDetail;
+
